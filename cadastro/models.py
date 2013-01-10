b27037486e245b9580a152734a09115915ed098f
Elton Santos <eltonmelosantos@hotmail.com>
2013-01-09 21:32:58 -0300

    Explorando a aplicação cadastro, fiz a tabela de clientes e tabela de clientedocliente e coloquei alguns campos

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+	nome = models.CharField(max_length=200)
+	email = models.CharField(max_length=200)
+	endereco = models.CharField(max_length=200)
+	telefone = models.CharField(max_length=200)
+
+class ClienteDoCliente(models.Model):
+	cliente = models.ForeignKey(Cliente)
+	nome = models.CharField(max_length=200)
+	email = models.CharField(max_length=200)
+	endereco = models.CharField(max_length=200)
+	telefone = models.CharField(max_length=200)
