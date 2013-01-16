5bf0e123d1bd8094b29ca9778e0656222a568516
Elton Santos <eltonmelosantos@hotmail.com>
2013-01-16 14:46:16 -0300

    Começando a comitar coisas do projeto, criei algumas aplicações

--- cobranca/urls.py
+++ cobranca/urls.py
@@ -11,10 +11,12 @@ urlpatterns = patterns('',
     # url(r'^cobranca/', include('cobranca.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^/', include(admin.site.urls)),
     #url(r'^$', include(admin.site.urls)),
+    url(r"^$", "login.views.login", name="home"),
+    url(r"^cadastro/", "cadastro.views.cadastro"),
+
 )
--- login/views.py
+++ login/views.py
@@ -2,5 +2,5 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
-def cadastro(request):
-	return render_to_response("cadastro.html")
+def login(request):
+	return render_to_response("login.html")
