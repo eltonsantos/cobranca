e28814b60ffda987405739aabf1c45cc16dda9af
System Administrator <root@MacBook-Pro-de-Laercio.local>
2013-02-21 15:43:37 -0300

    Alterado modo de conexão para novo SGDB PostgreSQL

--- cobranca/settings.py
+++ cobranca/settings.py
@@ -13,9 +13,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'cobranca',                      # Or path to database file if using sqlite3.
-        'USER': 'elton',                      # Not used with sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
